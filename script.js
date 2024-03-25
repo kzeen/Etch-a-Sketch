874efa299c98866a450f07e9e01d7979a6988111
zeeny <157370421+kzeen@users.noreply.github.com>
2024-03-25 14:08:25 +0200

    Add initial grid generation
    The default grid shown on the drawing board upon
    loading the page

--- script.js
+++ script.js
@@ -8,3 +8,28 @@ const resetButton  = document.querySelector(".reset-button");
 
 let currentColor = "";
 let currentDrawingMode = "";
+
+function getPixelSize(dimensions) {
+    // Calculates how much each pixel should be sized,
+    // according to its parent's size (100%)
+    return 100 / dimensions;
+}
+
+function generateGridPixel(pixelSize) {
+    const newGridPixel = document.createElement("div");
+    newGridPixel.style.border = "1px solid #000000";
+    newGridPixel.style.width = pixelSize + "%";
+    newGridPixel.style.height = pixelSize + "%";
+    grid.appendChild(newGridPixel);
+}
+
+function generateGrid(dimensions) {
+    // size: width & height  -  dimensions: int x int
+    // Generates a dimensions x dimensions grid
+    const pixelSize = getPixelSize(dimensions);
+    for (let i=0;i<(dimensions*dimensions);i++) {
+        generateGridPixel(pixelSize);
+    }
+}
+
+generateGrid(16);
