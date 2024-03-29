2f290268cd4050fd7716e6034824cf5b48c5d0bb
zeeny <157370421+kzeen@users.noreply.github.com>
2024-03-30 00:08:14 +0200

    Add grid reset functionality

--- script.js
+++ script.js
@@ -39,6 +39,10 @@ function initializePalette() {
         color.addEventListener("click", () => currentColor = color.className)
     });
     // Default selected color is the first in the palette
+    setDefaultColor();
+}
+
+function setDefaultColor() {
     currentColor = colorButtons[0].className;
 }
 
@@ -46,7 +50,6 @@ function initializePalette() {
 function setDrawingMode(e) {
     //Gets text content of clicked button
     const mode = e.target.textContent;
-
     if (mode !== currentDrawingMode) {
         currentDrawingMode = mode;
         drawingMode.textContent = currentDrawingMode;
@@ -65,6 +68,29 @@ function drawOnPixel(e) {
 function initializeButtons() {
     hoverButton.addEventListener("click", setDrawingMode);
     clickButton.addEventListener("click", setDrawingMode);
+    resetButton.addEventListener("click", reset);
+}
+
+//Reset 
+function reset() {
+    const newDimensions = getUserDimensions(100);
+    if (newDimensions === null) return;
+    grid.replaceChildren();
+    generateGrid(newDimensions);
+    setDefaultColor();
+    drawingMode.textContent = "-";
+    currentDrawingMode = "";
+}
+
+function getUserDimensions(maxValue) {
+    let dimensions = prompt("Enter new grid dimensions (max " + maxValue +")");
+    if (dimensions === null) return dimensions;
+    else {
+        while (isNaN(dimensions) || parseInt(dimensions) > 100) {
+            dimensions = prompt("Please enter a number less than 100");
+        }
+    }
+    return parseInt(dimensions);
 }
 
 function initializePage() {
